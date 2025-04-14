1182e28ba70d0b80567d1e5d6a7ed9c50bd839da
fabioserembe <fabio.serembe@algomera.it>
2025-04-14 09:57:48 +0200

    - Eliminazione tabella add_client_id_column_to_communications_table; - Risoluzione problemi foreign keys in rollback;

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -54,8 +55,10 @@ return new class extends Migration
      */
     public function down(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 };
