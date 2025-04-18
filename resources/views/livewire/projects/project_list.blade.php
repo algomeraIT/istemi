0b247ae7c0325fe44b9b68cac4a1464dfa72aaea
matteodenni <matteo.denni@algomera.it>
2025-04-18 10:16:36 +0200

    project-list style001

--- resources/views/livewire/projects/project_list.blade.php
+++ resources/views/livewire/projects/project_list.blade.php
@@ -1,32 +1,32 @@
 <div class="w-full overflow-x-auto">
     @if ($projects != null)
-        <table class="w-full bg-white border border-gray-300 shadow-md rounded-lg">
-            <thead>
-                <tr class=" text-gray-400">
-                    <th class="px-4 py-2 text-left">Pratica</th>
-                    <th class="px-4 py-2 text-left">Cliente</th>
-                    <th class="px-4 py-2 text-left">Tipo Cliente</th>
-                    <th class="px-4 py-2 text-left">Fase Progettuale</th>
-                    <th class="px-4 py-2 text-left">Responsabile</th>
-                    <th></th>
-                    <th></th>
+    <table class=" w-full min-w-[798px] font-inter text-sm text-left">
+        <thead class="text-[#B0B0B0] font-light text-[14px]">
+                <tr class="border-b h-10">
+                    <th class="w-[calc(100%/6)]">Preventivo</th>
+                    <th class="w-[calc(100%/6)]">Cliente</th>
+                    <th class="w-[calc(100%/6)]">Tipo Cliente</th>
+                    <th class="w-[calc(100%/6)]">Fase Progettuale</th>
+                    <th class="w-[calc(100%/6)]">Responsabile</th>
+                    <th class="flex justify-end">Azioni</th>
+                
                 </tr>
             </thead>
             <tbody>
 
                 @foreach ($projects as $project)
-                    <tr class="border-b hover:bg-gray-100">
-                        <td class="px-4 py-2">{{ $project->n_file }}</td>
-                        <td class="px-4 py-2">{{ $project->client_name }}</td>
-                        <td class="px-4 py-2 ">
+                <tr class="border-b h-12 text-[#232323] font-medium xl:text-lg lg:text-base md:text-sm sm:text-xs">
+                    <td class="  py-2">{{ $project->n_file }}</td>
+                        <td class="  py-2">{{ $project->client_name }}</td>
+                        <td class="  py-2 ">
                             <p>
                                 <span
-                                    class="px-2 py-1 text-xs font-semibold rounded {{ $project->client_type ? 'bg-gray-400' : 'bg-purple-400' }}">
+                                    class="px-2 py-1 text-xs font-semibold rounded {{ $project->client_type ? 'bg-[#F6F3F9]' : 'bg-[#F2F5F9]' }}">
                                     {{ $project->client_type ? 'Pubblico' : 'Privato' }}
                                 </span>
                             </p>
                         </td>
-                        <td class="px-4 py-2">
+                        <td class="  py-2">
                             <p>
                                 @php
                                     switch ($project->current_phase) {
@@ -54,25 +54,20 @@
                                 </span>
                             </p>
                         </td>
-                        <td class="px-4 py-2">{{ $project->responsible }}</td>
-                        <td class="px-4 py-2 w-2">
-                            <button wire:click="show({{ $project->id }})" title="Dettaglio"
-                                class="py-1  text-gray-600 rounded  hover:cursor-pointer">
-                                <flux:icon.eye />
-                            </button>
+                        <td class="  py-2">{{ $project->responsible }}</td>
+                        <td class="flex gap-2 mt-2.5 justify-end">
+                            @include('livewire.crm.utilities.detail-button', [
+                                'functionName' => 'show',
+                                'id' => $project->id,
+                            ])
+                            @include('livewire.crm.utilities.delete-button', [
+                                'functionName' => 'delete',
+                                'id' => $project->id,
+                            ])
+                            {{-- Optional: Edit Button --}}
+                            {{-- @include('livewire.crm.utilities.edit-button', ['functionName' => 'edit', 'id' => $lead->id]) --}}
                         </td>
-                        <td class="px-4 py-2 w-2">
-                            <button wire:click="edit({{ $project->id }})" title="Modifica"
-                                class=" py-1  text-gray-600 rounded  hover:cursor-pointer">
-                                <flux:icon.pencil-square />
-                            </button>
-                        </td>
-                        <td class="px-4 py-2 w-2">
-                            <button wire:click="delete({{ $project->id }})" title="Cancella"
-                                class=" py-1  text-gray-600 rounded  ml-2 hover:cursor-pointer">
-                                <flux:icon.x-mark />
-                            </button>
-                        </td>
+                
                     </tr>
                 @endforeach
 
